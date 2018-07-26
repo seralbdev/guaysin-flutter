79d0598388957e0ec86ad75995b8528044986057
seralbdev <seralbdev@gmail.com>
2018-07-26 18:10:27 +0200

    a bit of reestyling

--- lib/ui/siteList.dart
+++ lib/ui/siteList.dart
@@ -9,17 +9,21 @@ import 'package:guaysin/ui/loginPage.dart';
 import 'package:guaysin/ui/siteEditorPage.dart';
 import 'package:guaysin/services/cloudStorage.dart' as cloud;
 
-enum _PageMenuOptions { EXPORT_TO_CLOUD,IMPORT_FROM_CLOUD,EXPORT_TO_FILE,IMPORT_FROM_FILE }
+enum _PageMenuOptions {
+  EXPORT_TO_CLOUD,
+  IMPORT_FROM_CLOUD,
+  EXPORT_TO_FILE,
+  IMPORT_FROM_FILE
+}
 
-class SiteListPage extends StatefulWidget{ 
+class SiteListPage extends StatefulWidget {
   @override
   _SiteListPageState createState() {
-    return new _SiteListPageState(getCryptoServiceInstance()); 
+    return new _SiteListPageState(getCryptoServiceInstance());
   }
 }
 
 class _SiteListPageState extends State<SiteListPage> {
-
   final List<SiteData> _siteList = <SiteData>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   final _filterController = new TextEditingController();
@@ -31,7 +35,7 @@ class _SiteListPageState extends State<SiteListPage> {
 
   void _onAddNewSite() async {
     var localStorage = LocalStorage.get();
-    var sd = new SiteData('','','','');
+    var sd = new SiteData('', '', '', '');
     //await localStorage.saveSite(sd);
     //this.setState((){});
 
@@ -41,33 +45,32 @@ class _SiteListPageState extends State<SiteListPage> {
     );
   }
 
-  void _onTapOnSite(SiteData site){
+  void _onTapOnSite(SiteData site) {
     print(site.siteName);
 
     Navigator.push(
       context,
       new MaterialPageRoute(builder: (context) => new SiteEditorPage(site)),
     );
-
   }
 
-  Widget _buildListItem(SiteData site){
+  Widget _buildListItem(SiteData site) {
     return new ListTile(
-      title: new Text(site.siteName),
-      onTap: (){ _onTapOnSite(site);}
-    );
+        title: new Text(site.siteName),
+        onTap: () {
+          _onTapOnSite(site);
+        });
   }
 
   Future<Widget> _buildSiteList() async {
     var allSites = await LocalStorage.get().getAllSites();
     var items = new List<Widget>();
 
-    allSites.forEach((sd){
-      if(regexp.hasMatch(sd.siteName)){
+    allSites.forEach((sd) {
+      if (regexp.hasMatch(sd.siteName)) {
         var lt = _buildListItem(sd);
         items.add(lt);
       }
-
     });
 
     var lv = new ListView(
@@ -78,32 +81,29 @@ class _SiteListPageState extends State<SiteListPage> {
     return lv;
   }
 
-  void onFilter(){
+  void onFilter() {
     regexp = new RegExp(_filterController.text);
-    this.setState((){});
+    this.setState(() {});
   }
 
   void exportToCloud() async {
+    var result = await cloud.exportToCloud();
+    var msg = "Operation FAILED!!";
+    if (result) msg = "Operation succeeded";
 
-      var result = await cloud.exportToCloud();
-      var msg = "Operation FAILED!!";
-      if(result)
-        msg = "Operation succeeded";
-
-      _scaffoldKey.currentState.showSnackBar(new SnackBar(
-        content: new Text(msg),
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+      content: new Text(msg),
     ));
-
   }
 
   void importFromCloud() async {
     var result = await cloud.importFromCloud();
 
-    if(!result){
+    if (!result) {
       _scaffoldKey.currentState.showSnackBar(new SnackBar(
         content: new Text("Operation FAILED!"),
       ));
-    }else{
+    } else {
       Navigator.push(
         context,
         new MaterialPageRoute(builder: (context) => new LoginPage()),
@@ -112,14 +112,13 @@ class _SiteListPageState extends State<SiteListPage> {
   }
 
   void exportToFile() async {
-
-    try{
+    try {
       var localStorage = LocalStorage.get();
       await localStorage.exportDataToFile();
       _scaffoldKey.currentState.showSnackBar(new SnackBar(
         content: new Text("Operation SUCCEEDED!"),
       ));
-    }catch(ex){
+    } catch (ex) {
       _scaffoldKey.currentState.showSnackBar(new SnackBar(
         content: new Text("Operation FAILED!"),
       ));
@@ -135,16 +134,15 @@ class _SiteListPageState extends State<SiteListPage> {
         context,
         new MaterialPageRoute(builder: (context) => new LoginPage()),
       );
-
-    }catch(ex){
+    } catch (ex) {
       _scaffoldKey.currentState.showSnackBar(new SnackBar(
         content: new Text("Operation FAILED!"),
       ));
     }
   }
 
-  void popupMenuSelected(_PageMenuOptions valueSelected){
-    switch(valueSelected){
+  void popupMenuSelected(_PageMenuOptions valueSelected) {
+    switch (valueSelected) {
       case _PageMenuOptions.EXPORT_TO_CLOUD:
         exportToCloud();
         break;
@@ -162,83 +160,77 @@ class _SiteListPageState extends State<SiteListPage> {
 
   @override
   Widget build(BuildContext context) {
-    
-    var fb = new FutureBuilder( 
-      future: _buildSiteList(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData && snapshot.data != null) {
-
-            return new Column(
-                children: <Widget>[
-                  new Row(
-                      children: <Widget>[
-                        new RaisedButton(key:null, onPressed:onFilter,
-                            color: const Color(0xFFe0e0e0),
-                            child:
-                            new Text(
-                              "Filter",
-                              style: new TextStyle(fontSize:12.0,
-                                  color: const Color(0xFF000000),
-                                  fontWeight: FontWeight.w200,
-                                  fontFamily: "Roboto"),
-                            )
-                        ),
-                        new Flexible(
-                          child:new TextField(
-                            controller:_filterController,
-                            style: new TextStyle(fontSize:12.0,
-                            color: const Color(0xFF000000),
-                            fontWeight: FontWeight.w200,
-                            fontFamily: "Roboto"),
-                          )
-                        )
-                      ]
-                  ),
-                  new Expanded(child:
-                    snapshot.data
-                  )
-                ]);
+    var fb = new FutureBuilder(
+        future: _buildSiteList(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return new Column(children: <Widget>[
+              new Row(children: <Widget>[
+                new RaisedButton(
+                    key: null,
+                    onPressed: onFilter,
+                    color: const Color(0xFFe0e0e0),
+                    child: new Icon(Icons.search)),
+                new Flexible(
+                    child: new Container(
+                        margin: new EdgeInsets.all(3.0),
+                        child: new TextField(
+                            controller: _filterController,
+                            style: new TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.black,
+                                fontWeight: FontWeight.normal)
+                            //fontFamily: "Roboto"),
+                            )))
+              ]),
+              new Expanded(child: snapshot.data)
+            ]);
 
             //snapshot.data
-        }
-        return new Container();
-      });
+          }
+          return new Container();
+        });
 
     return new Scaffold(
-      key:_scaffoldKey,
-      appBar: new AppBar(
-        title: new Text('Site list'),
-        automaticallyImplyLeading: false,
-        actions: <Widget>[
-          // overflow menu
-          PopupMenuButton<_PageMenuOptions>(
-              onSelected: popupMenuSelected,
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<_PageMenuOptions>>[
-                const PopupMenuItem<_PageMenuOptions>(
-                  value: _PageMenuOptions.EXPORT_TO_CLOUD,
-                  child: const Text('ToCloud'),
-                ),
-                const PopupMenuItem<_PageMenuOptions>(
-                  value: _PageMenuOptions.IMPORT_FROM_CLOUD,
-                  child: const Text('FromCloud'),
-                ),
-                const PopupMenuItem<_PageMenuOptions>(
-                  value: _PageMenuOptions.EXPORT_TO_FILE,
-                  child: const Text('ToFile'),
-                ),
-                const PopupMenuItem<_PageMenuOptions>(
-                  value: _PageMenuOptions.IMPORT_FROM_FILE,
-                  child: const Text('FromFile'),
-                ),
-              ]
-          ),
-        ]),
-      body:fb,
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _onAddNewSite,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add)
-      )
-      );
+        key: _scaffoldKey,
+        appBar: new AppBar(
+            title: new Text('Site list'),
+            automaticallyImplyLeading: false,
+            actions: <Widget>[
+              // overflow menu
+              PopupMenuButton<_PageMenuOptions>(
+                  onSelected: popupMenuSelected,
+                  itemBuilder: (BuildContext context) =>
+                      <PopupMenuEntry<_PageMenuOptions>>[
+                        const PopupMenuItem<_PageMenuOptions>(
+                            value: _PageMenuOptions.EXPORT_TO_CLOUD,
+                            child: ListTile(
+                                leading: Icon(Icons.backup),
+                                title: const Text('ToCloud'))),
+                        const PopupMenuItem<_PageMenuOptions>(
+                          value: _PageMenuOptions.IMPORT_FROM_CLOUD,
+                          child: ListTile(
+                              leading: Icon(Icons.vertical_align_bottom),
+                              title: const Text('FromCloud')),
+                        ),
+                        const PopupMenuItem<_PageMenuOptions>(
+                          value: _PageMenuOptions.EXPORT_TO_FILE,
+                          child: ListTile(
+                              leading: Icon(Icons.file_upload),
+                              title: const Text('ToFile')),
+                        ),
+                        const PopupMenuItem<_PageMenuOptions>(
+                          value: _PageMenuOptions.IMPORT_FROM_FILE,
+                          child: ListTile(
+                              leading: Icon(Icons.file_download),
+                              title: const Text('FromFile')),
+                        ),
+                      ]),
+            ]),
+        body: fb,
+        floatingActionButton: new FloatingActionButton(
+            onPressed: _onAddNewSite,
+            tooltip: 'Increment',
+            child: new Icon(Icons.add)));
   }
 }
