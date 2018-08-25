c88ef7302f13228aa6d24712566906a3b569e6ea
seralbdev <seralbdev@gmail.com>
2018-08-25 17:25:59 +0200

    create first time page

---
+++ lib/ui/initPage.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:guaysin/services/cryptoServices.dart';
+import 'package:guaysin/ui/loginPage.dart';
+import 'package:guaysin/ui/siteList.dart';
+import 'package:guaysin/services/localStorage.dart';
+import 'package:guaysin/services/preferences.dart';
+import 'package:local_auth/local_auth.dart';
+
+class InitPage extends StatefulWidget {
+  @override
+  _InitPageState createState() => new _InitPageState();
+}
+
+class _InitPageState extends State<InitPage> {
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  final formKey = new GlobalKey<FormState>();
+
+  String _password;
+  String _token;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /*getCryptoServiceInstance().secretReady().then((flag) {
+      setState(() {
+        _secretReady = flag;
+      });*/
+  }
+
+  void _showErrorMessage(String msg) {
+    scaffoldKey.currentState.showSnackBar(new SnackBar(content: new Text(msg)));
+  }
+
+  void _submit() async {
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      try {
+        await _initializeApp();
+        Navigator.push(
+          context,
+          new MaterialPageRoute(builder: (context) => new LoginPage()),
+        );
+      } catch (ex) {
+        _showErrorMessage(ex);
+      }
+    }
+  }
+
+  Future _initializeApp() async {
+    var cs = getCryptoServiceInstance();
+    await cs.createSecret(_password);
+    await setUserToken(_token);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        key: scaffoldKey,
+        appBar: new AppBar(
+            title: new Text('Login page'), automaticallyImplyLeading: false),
+        body: new Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new Form(
+                key: formKey,
+                child: new Column(children: [
+                  new TextFormField(
+                    decoration: new InputDecoration(
+                        labelText: 'Introduce PASSWORD here...'),
+                    validator: (val) =>
+                        val.length < 6 ? 'Password too short.' : null,
+                    onSaved: (val) => _password = val,
+                    obscureText: true,
+                  ),
+                  new TextFormField(
+                      decoration: new InputDecoration(
+                          labelText: 'Introduce PASSWORD again...'),
+                      validator: (val) =>
+                          val.length < 6 ? 'Password too short.' : null,
+                      onSaved: (val) => _password = val,
+                      obscureText: true),
+                  new TextFormField(
+                    decoration: new InputDecoration(
+                        labelText: 'Introduce TOKEN here...'),
+                    validator: (val) =>
+                        val.isEmpty ? 'Must have a value' : null,
+                    onSaved: (val) => _token = val,
+                  ),
+                  new Container(
+                      margin: const EdgeInsets.all(30.0),
+                      child: new RaisedButton(
+                          onPressed: _submit, child: new Text('Start')))
+                ]))));
+  }
+}
